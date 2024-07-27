edfa78d63f01598a43c96c01bbe753618ec1df95
Lrmdom <lmatiasdomingos@gmail.com>
2024-07-27 16:40:48 +0100

    attributes field with intarray and objects

--- app/components/ExeclogService.tsx
+++ app/components/ExeclogService.tsx
@@ -14,7 +14,7 @@ export default function Service({taxonomies}: { taxonomy: SanityDocument }) {
     return (
         <main className="container mx-auto prose prose-lg p-4 border-4">
             {title ? (
-                <h1>
+                <h1 className='text-2xl'>
                     <b>{title}</b>
                 </h1>
             ) : null}
