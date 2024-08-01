69d84bb793935c3d195143f8482dbf41e4d38e7b
Lrmdom <lmatiasdomingos@gmail.com>
2024-08-01 18:39:01 +0100

    problem with cl react components

--- app/components/Prods.tsx
+++ app/components/Prods.tsx
@@ -4,39 +4,63 @@ import Variants from "~/components/Variants";
 import Attributes from "~/components/Attributes";
 import client from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
+import {Link} from "@remix-run/react";
 
 // Get a pre-configured url-builder from your sanity client
 const builder = imageUrlBuilder(client)
+
 function urlFor(source) {
     return builder.image(source)
 }
+
 export default function Prods({products}: { product: SanityDocument }) {
 
     return (
-        <main className="container mx-auto w-2/4 ">
+        <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-14 lg:max-w-7xl lg:px-8">
 
-            {products?.map((prod) => {
-                return (
-                    <>
-                        <div>
-                            <img src={prod.imageUrl} width={125} alt={prod.title}
-                            />
-                        </div>
-                        <div key={prod._id}>
 
-                            <span>
-                            {prod.title}
+                <div
+                    className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 
-                          </span>
-                            <Attributes product={prod}></Attributes>
-                        </div>
-                        <Variants product={prod}></Variants>
-                    </>
-                )
-            })}
-        </main>
+                    {products?.map((prod) => {
+                        return (
+                            <>
+
+                                <div  className="group relative">
+                                    <div  className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-48">
+
+                                        <img src={prod.imageUrl} width={75} alt={prod.title}
+                                             className="h-full w-full object-contain object-center lg:h-full lg:w-full"/>
+
+                                    </div>
+                                    <div className="mt-4 flex justify-between">
+                                        <div>
+                                            <h3 className="text-sm text-gray-700">
+                                                <Link to={`/${prod.taxon}/${prod.id}/${prod.title}`}> {stegaClean(prod.title)} </Link>
+
+                                            </h3>
+
+                                            {/*
+                                                    <p className="mt-1 text-sm text-gray-500">{prod.color}</p>
+*/}
+                                            <Attributes product={prod}></Attributes>
+                                        </div>
+                                        {/*
+                                                <p className="text-sm font-medium text-gray-900">{prod.price}</p>
+*/}
+                                        {/*<Variants product={prod}></Variants>*/}
+                                    </div>
+                                </div>
+
+
+                            </>
+                        )
+
+                    })}
+                </div>
+            </div>
+        </div>
+
     )
 }
-
-
-
