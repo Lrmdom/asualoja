f35d4712d7925f9d7edccd77411de72faf064fe1
Lrmdom <lmatiasdomingos@gmail.com>
2024-08-03 13:05:36 +0100

    routes related locale/taxonomy/taxon/slug

--- app/components/Variants.tsx
+++ app/components/Variants.tsx
@@ -60,44 +60,44 @@ export default function Variants({product}: { product: SanityDocument }) {
                     </div>
 
 
-                        {product.variants.map((variant) => {
-                            return (
-                                <>
-                                    <div key={variant._id} className=" flex container w-2/2 border-2 text-right">
-                                        <div className="justify-end">
-                                            <img src={variant.images ? variant.images[0].url : null} width={75}
-                                                 alt={variant.title}/>
-                                        </div>
-                                        <div>
-                                            <span>{variant.title}</span>
-                                            <span><Attributes product={variant}></Attributes></span>
-                                        </div>
-                                        <div>
-                                            <cl-price code={stegaClean(variant.sku)}>
-                                                <cl-price-amount type="compare-at"></cl-price-amount>
-                                                <cl-price-amount type="price"></cl-price-amount>
-                                            </cl-price>
-                                        </div>
-                                        <div>
-                                            <cl-add-to-cart code={stegaClean(variant.sku)} quantity="1" kind="sku">
-
-                                                {t('Add to cart')}
-                                            </cl-add-to-cart>
-                                        </div>
+                    {product.variants.map((variant) => {
+                        return (
+                            <>
+                                <div key={variant._id} className=" flex container w-2/2 border-2 text-right">
+                                    <div className="justify-end">
+                                        <img src={variant.images ? variant.images[0].url : null} width={75}
+                                             alt={variant.title}/>
                                     </div>
-                                    <br/>
-                                    <br/>
-                                    <br/>
-                                </>
-                            )
-                        })}
+                                    <div>
+                                        <span>{variant.title}</span>
+                                        <span><Attributes product={variant}></Attributes></span>
+                                    </div>
+                                    <div>
+                                        <cl-price code={stegaClean(variant.sku)}>
+                                            <cl-price-amount type="compare-at"></cl-price-amount>
+                                            <cl-price-amount type="price"></cl-price-amount>
+                                        </cl-price>
+                                    </div>
+                                    <div>
+                                        <cl-add-to-cart code={stegaClean(variant.sku)} quantity="1" kind="sku">
+
+                                            {t('Add to cart')}
+                                        </cl-add-to-cart>
+                                    </div>
+                                </div>
+                                <br/>
+                                <br/>
+                                <br/>
+                            </>
+                        )
+                    })}
 
 
                 </div>
             </div>
-                </>
-                )
-                }
+        </>
+    )
+}
 
 
 
