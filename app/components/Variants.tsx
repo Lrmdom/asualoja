be5130ffe4a1f85df9782bf0ca108740c51f8943
Lrmdom <lmatiasdomingos@gmail.com>
2024-08-04 11:48:49 +0100

    VARIANTS AND ATTIBUTES show corretcly and conditionaly

--- app/components/Variants.tsx
+++ app/components/Variants.tsx
@@ -13,90 +13,55 @@ import {Link} from "@remix-run/react";
     scope: 'market:id:vlkaZhkGNj'
 })*/
 
-export default function Variants({product}: { product: SanityDocument }) {
+export default function Variants({product}: { variants: SanityDocument }) {
 
     const {t} = useTranslation('')
 
-    if (Array.isArray(product.variants)) {
-        null
+    let displayVariants
+    if (Array.isArray(product.variants)){
+        return (
+            <>
+
+                {product.variants.map((variant) => {
+                    return (
+                        <>
+                            <div key={variant._id} className=" flex container w-2/2 text-right">
+                                <div className="justify-end">
+                                    <img src={variant.images ? variant.images[0].url : null} width={75}
+                                         alt={variant.title}/>
+                                </div>
+                                <div>
+                                    <span>{variant.title}</span>
+                                    <span><Attributes product={variant}></Attributes></span>
+                                </div>
+                                <div>
+                                    <cl-price code={stegaClean(variant.sku)}>
+                                        <cl-price-amount type="compare-at"></cl-price-amount>
+                                        <cl-price-amount type="price"></cl-price-amount>
+                                    </cl-price>
+                                </div>
+                                <div>
+                                    <cl-add-to-cart code={stegaClean(variant.sku)} quantity="1" kind="sku">
+
+                                        {t('Add to cart')}
+                                    </cl-add-to-cart>
+                                </div>
+                            </div>
+                            <br/>
+                            <br/>
+                            <br/>
+                        </>
+                    )
+                })}
+
+
+            </>
+        )
     } else {
         return null
     }
 
-    return (
-        <>
-            <div className="group relative">
-                <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-14 lg:max-w-7xl lg:px-8">
 
-
-                    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-
-                        <div
-                            className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-48">
-
-                            <img src={product.imageUrl} width={75} alt={product.title}
-                                 className="h-full w-full object-contain object-center lg:h-full lg:w-full"/>
-
-                            <div className="mt-4 flex justify-between">
-                                <div>
-                                    <h3 className="text-sm text-gray-700">
-                                        {/*    todo if taxon array use product.taxons[0]*/}
-                                        <Link
-                                            to={stegaClean(`/${product.taxonomy}/${product.taxons}/${product.title}`)}> {stegaClean(product.title)} </Link>
-
-                                    </h3>
-
-                                    {/*
-                                                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-*/}
-                                    <Attributes product={product}></Attributes>
-                                </div>
-                                {/*
-                                                <p className="text-sm font-medium text-gray-900">{product.price}</p>
-*/}
-                                {/*<Variants product={product}></Variants>*/}
-                            </div>
-                        </div>
-                    </div>
-
-
-                    {product.variants.map((variant) => {
-                        return (
-                            <>
-                                <div key={variant._id} className=" flex container w-2/2 border-2 text-right">
-                                    <div className="justify-end">
-                                        <img src={variant.images ? variant.images[0].url : null} width={75}
-                                             alt={variant.title}/>
-                                    </div>
-                                    <div>
-                                        <span>{variant.title}</span>
-                                        <span><Attributes product={variant}></Attributes></span>
-                                    </div>
-                                    <div>
-                                        <cl-price code={stegaClean(variant.sku)}>
-                                            <cl-price-amount type="compare-at"></cl-price-amount>
-                                            <cl-price-amount type="price"></cl-price-amount>
-                                        </cl-price>
-                                    </div>
-                                    <div>
-                                        <cl-add-to-cart code={stegaClean(variant.sku)} quantity="1" kind="sku">
-
-                                            {t('Add to cart')}
-                                        </cl-add-to-cart>
-                                    </div>
-                                </div>
-                                <br/>
-                                <br/>
-                                <br/>
-                            </>
-                        )
-                    })}
-
-
-                </div>
-            </div>
-        </>
-    )
 }
 
 
--- app/routes/$locale.$taxonomy.$taxons.$slug.tsx
+++ app/routes/$locale.$taxonomy.$taxons.$slug.tsx
@@ -1,5 +1,5 @@
 import type { LoaderFunctionArgs } from '@remix-run/node'
-import {useLoaderData, useRouteLoaderData} from '@remix-run/react'
+import {Link, useLoaderData, useRouteLoaderData} from '@remix-run/react'
 import type { SanityDocument } from '@sanity/client'
 
 import { loadQuery } from '~/sanity/loader.server'
@@ -10,6 +10,8 @@ import {
   PRODUCT_FILTEREDBY_TAXONOMY_TAXON_PRODUCTTITLE,
 } from '~/sanity/queries'
 import Variants from "~/components/Variants";
+import {stegaClean} from "@sanity/client/stega";
+import Attributes from "~/components/Attributes";
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const { data } = await loadQuery<SanityDocument>(
       PRODUCT_FILTEREDBY_TAXONOMY_TAXON_PRODUCTTITLE,
@@ -20,5 +22,42 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
 
 export default function ProductRoute() {
   const { data } = useLoaderData<typeof loader>()
-  return <Variants product={data.product}/>
+  console.log("leoooooo")
+  console.log(data.product)
+
+
+
+  return (
+      <>
+        <div className="group relative">
+          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-14 lg:max-w-7xl lg:px-8">
+            <h1>{data.product.title}</h1>
+            <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+
+              <div
+                  className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-48">
+
+                <img src={data.product.imageUrl} width={75} alt={data.product.title}
+                     className="h-full w-full object-contain object-center lg:h-full lg:w-full"/>
+
+
+              </div>
+
+            </div>
+            <div className="">
+              {data.product.description}
+            </div>
+            <div className="mt-4 flex justify-between border-2">
+              <Attributes product={data.product}></Attributes>
+            </div>
+            <div className="mt-4 flex justify-between">
+              <Variants product={data.product}></Variants>
+            </div>
+
+          </div>
+        </div>
+      </>
+  )
+
+
 }
