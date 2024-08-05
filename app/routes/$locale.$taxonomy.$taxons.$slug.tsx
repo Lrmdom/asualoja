8d33bac0f984809037e1b77f9a6d1f7aeecc2bf2
Lrmdom <lmatiasdomingos@gmail.com>
2024-08-05 23:01:45 +0100

    bug fix LINK to product detail from taxon inside taxon

--- app/routes/$locale.$taxonomy.$taxons.$slug.tsx
+++ app/routes/$locale.$taxonomy.$taxons.$slug.tsx
@@ -31,7 +31,7 @@ export default function ProductRoute() {
       <>
         <div className="group relative">
           <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-14 lg:max-w-7xl lg:px-8">
-            <h1>{data.product.title}</h1>
+            <h1 className="bg-primary p-4 rounded text-white">{data.product.title}</h1>
             <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 
               <div
