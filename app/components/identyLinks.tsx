e5da993141f50b41574dff53cc1fc3763eba646f
Lrmdom <lmatiasdomingos@gmail.com>
2024-08-06 16:05:55 +0100

    breadcrumb development using matches

--- app/components/identyLinks.tsx
+++ app/components/identyLinks.tsx
@@ -19,8 +19,8 @@ export default function IdentityLinks(props) {
     <NavigationMenuItem>
         <cl-my-account-link  type="signup" target="_self">
             <button
-                className="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2">Go
-                to my account
+                className="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2">
+                Go to my account
             </button>
         </cl-my-account-link>
     </NavigationMenuItem>
@@ -28,14 +28,14 @@ export default function IdentityLinks(props) {
     <NavigationMenuItem>
         <cl-identity-status type="guest">
             <button
-                className="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2">Register
-                now!
+                className="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2">
+                Register now!
             </button>
         </cl-identity-status>
         <cl-identity-status type="customer">
             <button
-                className="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2">Welcome
-                back!
+                className="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2">
+                Welcome back!
             </button>
         </cl-identity-status>
     </NavigationMenuItem>
