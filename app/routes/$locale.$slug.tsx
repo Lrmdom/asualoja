cef3acea4c2982bb0ae09a67034457b8d96d5e2d
Lrmdom <lmatiasdomingos@gmail.com>
2024-08-06 08:21:47 +0100

    breadcrumb development using matches

--- app/routes/$locale.$slug.tsx
+++ app/routes/$locale.$slug.tsx
@@ -1,5 +1,5 @@
 import type { LoaderFunctionArgs } from '@remix-run/node'
-import {useLoaderData, useRouteLoaderData} from '@remix-run/react'
+import {Link, useLoaderData, useRouteLoaderData} from '@remix-run/react'
 import type { SanityDocument } from '@sanity/client'
 
 import Service from '~/components/ExeclogService'
@@ -12,6 +12,14 @@ import {
   TAXONOMY_PRODS_ATTRS_VARIANTS_ATTRS_QUERY_LOCALIZED
 } from '~/sanity/queries'
 import i18next from '~/i18next.server'
+
+import { BreadcrumbsItem } from "~/components/BreadcrumbsItem"
+
+export const handle = {
+  breadcrumb: () => (
+      <Link to="/parent/child">Child Route</Link>
+  ),
+};
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const { data } = await loadQuery<SanityDocument>(
