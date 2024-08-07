d17e245a9fa2153269ce43939411d84f41ce9781
Lrmdom <lmatiasdomingos@gmail.com>
2024-08-07 04:22:53 +0100

    product page and variant page. Merge and group attributes to display and select accordingly

--- app/components/variantAttributes.tsx
+++ app/components/variantAttributes.tsx
@@ -2,9 +2,8 @@ import type {SanityDocument} from '@sanity/client'
 import {stegaClean} from "@sanity/client/stega"
 
 export default function VariantAttributes({attributes}: { attribute: SanityDocument }) {
-    console.log(attributes)
     //todo , group and display by attribute name. On select attributes, apply sku code data to <cl-price> and <cl-add-to-cart>
-
+console.log(attributes)
     if (Array.isArray(attributes)) {
         return (
             <main className="border border-primary border-1 rounded p-2 w-60">
