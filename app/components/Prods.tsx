c8abee20bd65722a6653f00953e62f3002ec754f
Lrmdom <lmatiasdomingos@gmail.com>
2024-08-09 16:23:50 +0100

    TABS FOR TAXONS- problem remains when taxon have products an d first subtaxon also have.

--- app/components/Prods.tsx
+++ app/components/Prods.tsx
@@ -6,21 +6,23 @@ import {Link} from "@remix-run/react";
 import {useTranslation} from "react-i18next";
 
 export default function Prods({products}: { product: SanityDocument }) {
-
+    console.log(products)
     const {i18n} = useTranslation()
     const language = i18n.resolvedLanguage
+
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-14 lg:max-w-7xl lg:px-8">
                 <div
                     className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {products?.map((prod) => {
+
                         return (
                             <>
                                 <div className="group relative">
                                     <div
                                         className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-48">
-
+{/*//TODO join variants images to product images to create a image slider*/}
                                         <img src={prod.imageUrl} width={75} alt={prod.title}
                                              className="h-full w-full object-contain object-center lg:h-full lg:w-full"/>
                                     </div>
