ed8a40d2b8679d61798fa167347286f99290b9f4
Lrmdom <lmatiasdomingos@gmail.com>
2024-08-16 14:24:52 +0100

    visualization of attributes with problems

--- app/components/variantAttributes.tsx
+++ app/components/variantAttributes.tsx
@@ -5,13 +5,13 @@ import Attribute from "~/components/attribute";
 export default function VariantAttributes({attributes}: { attribute: SanityDocument }) {
     //todo , group and display by attribute name. On select attributes, apply sku code data to <cl-price> and <cl-add-to-cart>
     // todo , use new es to destructure attributes object
-    //console.log(attributes)
+
     return (
         <main className="">
             {Object.entries(attributes).map((attribute) => {
                 return (
                     <>
-
+                        <h2>{attribute[0]}</h2>
                         <Attribute attribute={attribute[1]}></Attribute>
 
                     </>
