c236bc30bd1f1eabc382919753e3036ce5ed31b3
Lrmdom <lmatiasdomingos@gmail.com>
2024-08-28 17:27:29 +0100

    select and hide atributes bsed on image- aproach 1

--- app/components/attribute.tsx
+++ app/components/attribute.tsx
@@ -13,9 +13,11 @@ export default function Attribute({attribute}: { attribute: SanityDocument }) {
     if (stegaClean(attribute[0].visualPresentation?.visualization) === "InputToggleButton") {
         return (
             attribute.map((attr) => {
+                const output = `rounded border border-primary p-2 ${attr.sku}`
+
                 return (
                     <div>
-                        <button className="rounded border border-primary p-2" value={attr.value}
+                        <button className={output} value={stegaClean(attr.value)}
                                 name="size"> {stegaClean(attr.value.toUpperCase())}</button>
                         {attr.description ?
 
@@ -88,11 +90,13 @@ export default function Attribute({attribute}: { attribute: SanityDocument }) {
         let Reg_Exp = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i;
         return (
             attribute.map((attr) => {
-                let output
+                let output = `rounded border border-primary p-2 ${attr.sku}`
                 Reg_Exp.test(stegaClean(attr.value)) ?   //is valid color?
                     output = <AttributeVisualization attribute={attr}/>
                     :
-                    output = <button className="rounded border border-primary p-2" value={attr.value} name="size"> {stegaClean(attr.value.toUpperCase())}</button>
+                    output =
+                        <button className={output} value={stegaClean(attr.value)}
+                                name="size"> {stegaClean(attr.value.toUpperCase())}</button>
                 return (
                     <div>
                         {output}
