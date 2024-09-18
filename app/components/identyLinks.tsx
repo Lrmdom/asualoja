54bdc51dea0b0b17e7478d83c43567d75c01dc7d
Lrmdom <lmatiasdomingos@gmail.com>
2024-09-18 09:51:33 +0100

    workingvisual editing----try to fix

--- app/components/identyLinks.tsx
+++ app/components/identyLinks.tsx
@@ -12,6 +12,15 @@ export default function IdentityLinks(props) {
         <ul className="ml-10 flex items-baseline space-x-4">
 
             <li>
+                <cl-identity-link type="login" target="_self">
+                    <button
+                        className="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2">
+                        {t("Identity link")}
+
+                    </button>
+                </cl-identity-link>
+            </li>
+            <li>
                 <cl-my-account-link type="signup" target="_self">
                     <button
                         className="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2">
