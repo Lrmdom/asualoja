8360ebef8495db294fe2144c53d0ca1799c165f7
Lrmdom <lmatiasdomingos@gmail.com>
2024-10-07 06:07:50 +0100

    socials logins refatoring

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -2,163 +2,180 @@ import {Authenticator} from 'remix-auth'
 import {sessionStorage} from '~/services/session.server'
 // Create an instance of the authenticator, pass a generic with what
 // strategies will return and will store in the session
-/*import { FormStrategy } from 'remix-auth-form'
-import { GitHubStrategy } from 'remix-auth-github'
-import { GoogleStrategy } from 'remix-auth-google'
-import { FacebookStrategy } from 'remix-auth-facebook'
-import { LinkedinStrategy } from 'remix-auth-linkedin'
-import { Twitter2Strategy } from 'remix-auth-twitter'
-import { MicrosoftStrategy } from 'remix-auth-microsoft'
-import TwitterApi from 'twitter-api-v2'*/
+import {FormStrategy} from 'remix-auth-form'
+import {GitHubStrategy} from 'remix-auth-github'
+import {GoogleStrategy} from 'remix-auth-google'
+import {FacebookStrategy} from 'remix-auth-facebook'
+import {Twitter2Strategy} from 'remix-auth-twitter'
+import {MicrosoftStrategy} from 'remix-auth-microsoft'
+import TwitterApi from 'twitter-api-v2'
+
+import {LinkedinStrategy} from 'remix-auth-linkedin'
 
 export let authenticator = new Authenticator(sessionStorage)
 
-/*authenticator.use(
-  new MicrosoftStrategy(
-    {
-      clientId: 'db21c78b-1f73-4c45-a8f0-982a2f49f287',
-      clientSecret: 'RPv8Q~i7ViKaN8qbQsoCE2650ELw83U4.XSz6dBD',
-      redirectUri: 'http://localhost:5173/auth/microsoft/callback',
-      tenantId: '9b986352-978e-4025-97a5-2af66c8022de', // optional - necessary for organization without multitenant (see below)
-      scope: 'openid profile email', // optional
-      prompt: 'login', // optional
-    },
-    async ({ accessToken, extraParams, profile }) => {
-      // Here you can fetch the user from database or return a user object based on profile
-      // return {profile}
-      // The returned object is stored in the session storage you are using by the authenticator
-
-      // If you're using cookieSessionStorage, be aware that cookies have a size limit of 4kb
-      // For example this won't work
-      // return {accessToken, extraParams, profile}
-
-      // Retrieve or create user using id received from userinfo endpoint
-      // https://graph.microsoft.com/oidc/userinfo
-
-      // DO NOT USE EMAIL ADDRESS TO IDENTIFY USERS
-      // The email address received from Microsoft Entra ID is not validated and can be changed to anything from Azure Portal.
-      // If you use the email address to identify users and allow signing in from any tenant (`tenantId` is not set)
-      // it opens up a possibility of spoofing users!
-
-      //return User.findOrCreate({ id: profile.id });
-
-      return profile
-    }
-  )
+authenticator.use(
+    new LinkedinStrategy(
+        {
+            clientID: '77sukk301f74bf',
+            clientSecret: 'liwPtmOReOVePGKd',
+            // LinkedIn is expecting a full URL here, not a relative path
+            // see: https://learn.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow?tabs=HTTPS1#step-1-configure-your-application
+            callbackURL: 'http://localhost:5173/auth/linkedin/callback',
+        },
+        async ({accessToken, refreshToken, extraParams, profile, context}) => {
+            return profile
+        }
+    ),
+    'linkedin'
 )
 
 authenticator.use(
-  new Twitter2Strategy(
-    {
-      clientID: 'MUpJSU1HcmwxcXQ1Nm45YmFFeGw6MTpjaQ',
-      clientSecret: 'rKuyvkfMDGhSrGtQ_vUWri9xpc8DfPXIBi4nqVNmyc4Q6yRDah',
-      callbackURL: 'http://localhost:5173/auth/twitter/callback',
-      // List of scopes you want to be granted. See
-      scopes: ['users.read', 'tweet.read', 'tweet.write'],
-    },
-    // Define what to do when the user is authenticated
-    async ({ accessToken }) => {
-      //return profile
-      // In this example I want to use Twitter as identity provider: so resolve identity from the token
-      const userClient = new TwitterApi(accessToken)
+    new MicrosoftStrategy(
+        {
+            clientId: 'db21c78b-1f73-4c45-a8f0-982a2f49f287',
+            clientSecret: 'RPv8Q~i7ViKaN8qbQsoCE2650ELw83U4.XSz6dBD',
+            redirectUri: 'http://localhost:5173/auth/microsoft/callback',
+            tenantId: '9b986352-978e-4025-97a5-2af66c8022de', // optional - necessary for organization without multitenant (see below)
+            scope: 'openid profile email', // optional
+            prompt: 'login', // optional
+        },
+        async ({accessToken, extraParams, profile}) => {
+            // Here you can fetch the user from database or return a user object based on profile
+            // return {profile}
+            // The returned object is stored in the session storage you are using by the authenticator
 
-      const result = await userClient.v2.me({
-        'user.fields': ['profile_image_url'],
-      })
-      // should handle errors
-      const { id, username } = result.data
-      return result.data
-      // Return a user object to store in sessionStorage.
-      // You can also throw Error to reject the login
-      /!*return await registerUser(
-                                            accessToken,
-                                            id,
-                                            username
-                                          );*!/
-    }
-  ),
-  'twitter'
+            // If you're using cookieSessionStorage, be aware that cookies have a size limit of 4kb
+            // For example this won't work
+            // return {accessToken, extraParams, profile}
+
+            // Retrieve or create user using id received from userinfo endpoint
+            // https://graph.microsoft.com/oidc/userinfo
+
+            // DO NOT USE EMAIL ADDRESS TO IDENTIFY USERS
+            // The email address received from Microsoft Entra ID is not validated and can be changed to anything from Azure Portal.
+            // If you use the email address to identify users and allow signing in from any tenant (`tenantId` is not set)
+            // it opens up a possibility of spoofing users!
+
+            //return User.findOrCreate({ id: profile.id });
+
+            return profile
+        }
+    )
 )
 
 authenticator.use(
-  new LinkedinStrategy(
-    {
-      clientID: '77sukk301f74bf',
-      clientSecret: 'liwPtmOReOVePGKd',
-      // LinkedIn is expecting a full URL here, not a relative path
-      // see: https://learn.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow?tabs=HTTPS1#step-1-configure-your-application
-      callbackURL: 'http://localhost:5173/auth/linkedin/callback',
-    },
-    async ({ accessToken, refreshToken, extraParams, profile, context }) => {
-      return profile
-    }
-  ),
-  'linkedin'
+    new Twitter2Strategy(
+        {
+            clientID: 'MUpJSU1HcmwxcXQ1Nm45YmFFeGw6MTpjaQ',
+            clientSecret: 'rKuyvkfMDGhSrGtQ_vUWri9xpc8DfPXIBi4nqVNmyc4Q6yRDah',
+            callbackURL: 'http://localhost:5173/auth/twitter/callback',
+            // List of scopes you want to be granted. See
+            scopes: ['users.read', 'tweet.read', 'tweet.write'],
+        },
+        // Define what to do when the user is authenticated
+        async ({accessToken}) => {
+            //return profile
+            // In this example I want to use Twitter as identity provider: so resolve identity from the token
+            const userClient = new TwitterApi(accessToken)
+
+            const result = await userClient.v2.me({
+                'user.fields': ['profile_image_url'],
+            })
+            // should handle errors
+            const {id, username} = result.data
+            return result.data
+            // Return a user object to store in sessionStorage.
+            // You can also throw Error to reject the login
+            return await registerUser(
+                accessToken,
+                id,
+                username
+            );
+        }
+    ),
+    'twitter'
 )
 
 authenticator.use(
-  new GoogleStrategy(
-    {
-      clientID:
-        '1091535953121-mb4b5ap4uij06s5nqmcbpia3mpdo4437.apps.googleusercontent.com',
-      clientSecret: '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',
-      //callbackURL: "http://localhost:5173/auth/google/callback"
-      callbackURL: 'http://localhost:5173/auth/google/callback',
-    },
-    async ({ accessToken, refreshToken, extraParams, profile }) => {
-      // here you would find or create a user in your database
-      //return User.findOrCreate({ email: profile.emails[0].value })
-
-      return profile
-    }
-  ),
-  'google'
+    new LinkedinStrategy(
+        {
+            clientID: '77sukk301f74bf',
+            clientSecret: 'liwPtmOReOVePGKd',
+            // LinkedIn is expecting a full URL here, not a relative path
+            // see: https://learn.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow?tabs=HTTPS1#step-1-configure-your-application
+            callbackURL: 'http://localhost:5173/auth/linkedin/callback',
+        },
+        async ({accessToken, refreshToken, extraParams, profile, context}) => {
+            return profile
+        }
+    ),
+    'linkedin'
 )
 
 authenticator.use(
-  new FacebookStrategy(
-    {
-      clientID: '1031654645049512',
-      clientSecret: 'a47b82c7a365970bca801ebde865adaa',
-      callbackURL: 'http://localhost:5173/auth/facebook/callback',
-    },
-    async ({ profile }) => {
-      return profile
-    }
-  ),
-  'facebook'
+    new GoogleStrategy(
+        {
+            clientID:
+                '1091535953121-mb4b5ap4uij06s5nqmcbpia3mpdo4437.apps.googleusercontent.com',
+            clientSecret: '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',
+            //callbackURL: "http://localhost:5173/auth/google/callback"
+            callbackURL: 'http://localhost:5173/auth/google/callback',
+        },
+        async ({accessToken, refreshToken, extraParams, profile}) => {
+            // here you would find or create a user in your database
+            //return User.findOrCreate({ email: profile.emails[0].value })
+
+            return profile
+        }
+    ),
+    'google'
+)
+
+authenticator.use(
+    new FacebookStrategy(
+        {
+            clientID: '1031654645049512',
+            clientSecret: 'a47b82c7a365970bca801ebde865adaa',
+            callbackURL: 'http://localhost:5173/auth/facebook/callback',
+        },
+        async ({profile}) => {
+            return profile
+        }
+    ),
+    'facebook'
 )
 
 //it is an EXECLOG acount
 authenticator.use(
-  new GitHubStrategy(
-    {
-      clientID: 'Ov23liCky8imGJxf1sfQ',
-      clientSecret: '8294ca168ce1aab0bcf50a300d865f40929f6768',
-      callbackURL: 'http://localhost:5173/auth/github/callback',
-    },
-    async ({ accessToken, extraParams, profile }) => {
-      // Save/Get the user data from your DB or API using the tokens and profile
+    new GitHubStrategy(
+        {
+            clientID: 'Ov23liCky8imGJxf1sfQ',
+            clientSecret: '8294ca168ce1aab0bcf50a300d865f40929f6768',
+            callbackURL: 'http://localhost:5173/auth/github/callback',
+        },
+        async ({accessToken, extraParams, profile}) => {
+            // Save/Get the user data from your DB or API using the tokens and profile
 
-      return profile
-    }
-  ),
-  'github'
-)*/
+            return profile
+        }
+    ),
+    'github'
+)
 
 
-/*// Tell the Authenticator to use the form strategy
-authenticator.use(
-  new FormStrategy(async ({ form }) => {
-    let email = form.get('email')
-    let password = form.get('password')
-    let user = await login(email, password)
-    // the type of this user must match the type you pass to the Authenticator
-    // the strategy will automatically inherit the type if you instantiate
-    // directly inside the `use` method
-    return user
-  }),
-  // each strategy has a name and can be changed to use another one
-  // same strategy multiple times, especially useful for the OAuth2 strategy.
-  'user-pass'
+// Tell the Authenticator to use the form strategy
+/*authenticator.use(
+    new FormStrategy(async ({form}) => {
+        let email = form.get('email')
+        let password = form.get('password')
+        let user = await login(email, password)
+        // the type of this user must match the type you pass to the Authenticator
+        // the strategy will automatically inherit the type if you instantiate
+        // directly inside the `use` method
+        return user
+    }),
+    // each strategy has a name and can be changed to use another one
+    // same strategy multiple times, especially useful for the OAuth2 strategy.
+    'user-pass'
 )*/
