99c7a921a4d09cc82b4bd6bc60b15eed270e94c1
Lrmdom <lmatiasdomingos@gmail.com>
2025-02-11 18:10:51 +0000

    done- groq-js queries work with parameters

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20-alpine
+FROM node:18.20.1
 #ENV NODE_ENV development
 WORKDIR /app
 COPY package.json .
@@ -8,7 +8,7 @@ COPY . .
 RUN npm run build
 EXPOSE 3000
 CMD [ "npm", "run", "start" ]
-#docker build -t execlog/execlogdemo -f prod.Dockerfile .  && docker run -p 80:3000 -it foo
+#docker build -t foo .  && docker run -p 80:3000 -it foo
 
 # works on gcp cloud run
-#docker buildx build --platform=linux/amd64 -t execlog/execlogdemo555 --push .
+#docker buildx build --platform=linux/amd64 -t execlog/execlogdemo --push .
--- app/routes/$locale.$taxonomy.$taxon.datasetExportFromDocumentsChangeOrCreate.ts
+++ app/routes/$locale.$taxonomy.$taxon.datasetExportFromDocumentsChangeOrCreate.ts
@@ -1,11 +1,14 @@
 import type {LoaderFunctionArgs} from "@remix-run/node"; // or cloudflare/deno
-import {json} from "@remix-run/node";
-import {createClient, type SanityDocument} from "@sanity/client";
+import {createClient} from "@sanity/client";
 
 import {dataset, projectId, stegaEnabled, studioUrl} from "../sanity/projectDetails";
+import {Storage} from "@google-cloud/storage";
+import {evaluate, parse} from 'groq-js'
+
+
+import {TAXONOMY_QUERY_LOCALIZED_local} from '~/sanity/queries'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const exportDataset = require('@sanity/export')
 
 const client = createClient({
     projectId,
@@ -18,44 +21,67 @@ const client = createClient({
     },
 });
 
-const { curly } = require('node-libcurl')
-
-const fs = require('fs');
-const request = require('request');
-
-const { mkdir } = require("fs/promises");
-const { Readable } = require('stream');
-const { finished } = require('stream/promises');
-const path = require("path");
-
-import {parse, evaluate} from 'groq-js'
-
-
-import {PRODUCT_FILTEREDBY_TAXONOMY_TAXON_LOCALIZED} from '~/sanity/queries'
+import fs from "fs"
 
 
 export const loader = async ({
-                                 request,params
+                                 request, params
                              }: LoaderFunctionArgs) => {
 
-
-
-
-    const {locale,slug}  = {...params}
-//const input=`*[_type == "taxonomy" && title[_key == "${locale}"][0].value == "${slug}"][0]`
-    const input=PRODUCT_FILTEREDBY_TAXONOMY_TAXON_LOCALIZED
+    //const input=`*[_type == "taxonomy" && title[_key == $locale][0].value == $taxonomy]`
+    const input = TAXONOMY_QUERY_LOCALIZED_local
 // Returns an ESTree-inspired syntax tree
-    let tree = parse(input,{params})
+    let tree = parse(input, {params})
 
-    let resp =  await fetch('https://ho1tf79n.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+in+%5B%22taxon%22%2C+%22taxonomy%22%2C%22product%22%2C%22variant%22%5D%5D&perspective=published')
+    let resp = await fetch('https://ho1tf79n.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%22taxonomy%22%5D%0A%0A+%7B++_type%2C%0A++++%22title%22%3A+title%2C%0A++++%22imageUrl%22%3A+image.asset-%3Eurl+%2B+%22%3Fauto%3Dformat%26w%3D250%22%2C%0A++++taxons%5B%5D-%3E%7B%0A++++++_type%2C%0A++++++%22title%22%3A+title%2C%0A++++++++taxons%5B%5D-%3E%7B%0A++++++++++_type%2C%0A++++++++++++%22title%22%3A+title%2C%0A++++++++++++products%5B%5D-%3E%7B%22imageUrl%22%3A+image.asset-%3Eurl+%2B+%22%3Fauto%3Dformat%26w%3D250%22%2C%0A++++++++++++%22title%22%3A+title%2C%0A++++++++++++++++%22attributes%22%3A+tit%2C%0A++++++++++++++++variants%5B%5D-%3E%7B+%0A++++++++++++++++sku%2C%0A++++++++++++++++%22images%22%3A+images%5B%5D%7B%0A++++++++++++++++++++++++++%27url%27%3A+asset-%3Eurl+%2B+%22%3Fauto%3Dformat%26w%3D250%22%2C%0A++++++++++++++++++++++++++%7D%2C%0A++++++++++++++++++++%22title%22%3A+title%2C%0A++++++++++++++++++++%22attributes%22%3A+%0A++++++++++++++++++++++++attributes++++++++%0A++++++++++++++++%7D+++%0A++++++++++++%7D+%0A++++++++%7D%2C%0A++++++++products%5B%5D-%3E%7B%22imageUrl%22%3A+image.asset-%3Eurl+%2B+%22%3Fauto%3Dformat%26w%3D250%22%2C%0A++++++++++++%22title%22%3A+title%2C%0A++++++++++++%22attributes%22%3A+%0A++++++++++++++++++++attributes+%2C%0A++++++++++++variants%5B%5D-%3E%7B+sku%2C%0A++++++++++++%22images%22%3A+images%5B%5D%7B%0A++++++++++++++++++++%27url%27%3A+asset-%3Eurl+%2B+%22%3Fauto%3Dformat%26w%3D250%22%2C%0A++++++++++++++++++++%7D%2C%0A++++++++++++++++++++%22title%22%3A+title+%2C%0A++++++++++++++++%22attributes%22%3A+%0A++++++++++++attributes%0A++++++++++++++++++++++++++++++++++++++++++++++++++++++++++%0A++++++++++++%7D%0A+++++++++%7D%0A++++%7D+%0A+%7D%0A++&%24locale=%22en%22&%24taxon=%22Road%22&%24taxonomy=%22Bicycles%22&perspective=published')
     const jsn = await resp.json()
-    const filename=Date.now()+"myfiledata.json"
 
-    fs.writeFileSync(filename, JSON.stringify(jsn.result));
+    const filename = "myfiledata.json"
 
+    fs.writeFileSync(filename, JSON.stringify(jsn.result), {encoding: 'utf8', flag: 'w'});
 
-    const dataset=JSON.parse(await fs.readFileSync(filename,{ encoding: 'utf8', flag: 'r' }));
-// Evaluate a tree against a dataset
+    const storage = new Storage({
+        projectId: "avid-infinity-370500",
+        keyFilename: "service-account.json",
+    });
+
+    const uploadToFirebaseStorage = async (uploadFilePath, fileName) => {
+        try {
+            const gcs = storage.bucket("sanity_production_dataset"); // Removido "gs://" do nome do bucket
+            const result = await gcs.upload(fileName, {
+                destination: uploadFilePath,
+                /*predefinedAcl: 'publicRead', // Defina o arquivo para ser legível publicamente
+                metadata: {
+                    contentType: "application/main", // Ajuste o tipo de conteúdo conforme necessário
+                }*/
+            });
+            return result[0].metadata.mediaLink;
+        } catch (error) {
+            console.log(error);
+            throw new Error(error.message);
+        }
+    }
+
+    const uploadfilepath = `dataset/${filename}`
+
+    const link = await uploadToFirebaseStorage(uploadfilepath, filename);
+    console.log(link);
+
+    /*const downloadAsJson =  async () => {
+        const file = new Storage()
+            .bucket("sanity_production_dataset")
+            .file(`dataset/${filename}`)
+        return file.cloudStorageURI.href;
+    }*/
+    const file = new Storage()
+        .bucket("sanity_production_dataset")
+        .file(`dataset/${filename}`).cloudStorageURI.href
+
+    console.log(file)
+
+    const dataset = JSON.parse(await fs.readFileSync(filename, {encoding: 'utf8', flag: 'r'}));
+
+    // Evaluate a tree against a dataset
     let value = await evaluate(tree, {dataset})
 
 // Gather everything into one JavaScript object
@@ -63,132 +89,4 @@ export const loader = async ({
 
     return {result}
 
-    /* const resp = await fetch('https://ho1tf79n.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+in+%5B%22taxon%22%2C+%22taxonomy%22%2C%22product%22%2C%22variant%22%5D%5D&perspective=published');
-
-    const dataset = await resp.json();
-
-    console.log(dataset)
-
-    let input = '*[_type == "taxon"]'
-
-// Returns an ESTree-inspired syntax tree
-    let tree = parse(input)
-
-// Evaluate a tree against a dataset
-    let value = await evaluate(tree, {dataset})
-
-// Gather everything into one JavaScript object
-    let result = await value.get()*/
-
-
-
- /*   const downloadFile = (async (url, fileName) => {
-
-        const res = await fetch(url);
-        if (!fs.existsSync("downloads")) await mkdir("downloads"); //Optional if you already have downloads directory
-        const destination = path.resolve("./downloads", fileName);
-        const fileStream = fs.createWriteStream(destination, { flags: 'wx'});
-        await finished(Readable.fromWeb(res.body).pipe(fileStream));
-    });
-
-    await downloadFile("https://ho1tf79n.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+in+%5B%22taxon%22%2C+%22taxonomy%22%2C%22product%22%2C%22variant%22%5D%5D&%24locale=%22pt%22&%24taxons=%22Estrada%22&%24taxonomy=%22Bicicletas%22&%24slug=%22Bicicleta+estrada+trek+Madone+SL+77Gen+8%22&perspective=published", Date.now()+"myfiledata.ndjson")
-
-*/
-
-    /* const { data } = await curly.post('https://ho1tf79n.api.sanity.io/v2021-06-07/data/export/production', {
-         //postFields: JSON.stringify({ field: 'value' }),
-         httpHeader: [
-             'Content-Type: application/json',
-             'Accept: application/json',
-             'Authorization: Bearer sknWtK4sRtdBnGntwSiu8QA4bjYRark5Zh5c9xxKdOP7CaXbdVQeOYAHJSVNt4OGbOjAQIYcWuaCE36TdnuL01UyUv5N0zlTQC7vfxrjSqJY4GLRjobBsAoBKzN7kcl6wPrkR8AIi0BN6Wz3VucDNrct0CrzyMIZiddRDyHC4fIpEvZJibOj'
-         ],
-     })
-
-     console.log(data)*/
-    /*const { exec } = require('child_process');
-
-    exec('curl -H "Authorization: Bearer sknWtK4sRtdBnGntwSiu8QA4bjYRark5Zh5c9xxKdOP7CaXbdVQeOYAHJSVNt4OGbOjAQIYcWuaCE36TdnuL01UyUv5N0zlTQC7vfxrjSqJY4GLRjobBsAoBKzN7kcl6wPrkR8AIi0BN6Wz3VucDNrct0CrzyMIZiddRDyHC4fIpEvZJibOj" "https://ho1tf79n.api.sanity.io/v2021-06-07/data/export/production" > mydataexported.ndjson');
-*/
-
-
-
-/*// Define the URL you want to request
-    const url = 'https://ho1tf79n.api.sanity.io/v2021-06-07/data/export/production'; // Example URL
-
-// Make a GET request to the URL
-    request(url, (error, response, body) => {
-        if (error) {
-            console.error('Error occurred:', error);
-            return;
-        }
-
-        // Check if the request was successful (status code 200)
-        if (response.statusCode === 200) {
-            // Save the response data (body) to a file
-            fs.writeFile('responseData.json', body, (err) => {
-                if (err) {
-                    console.error('Error writing to file:', err);
-                } else {
-                    console.log('Response data saved to responseData.json');
-                }
-            });
-        } else {
-            console.log('Request failed with status code:', response.statusCode);
-        }
-    });*/
-
-
-/*
-    const client = createClient({
-        projectId,
-        dataset,
-        useCdn: true,
-        apiVersion: "2024-06-15",
-        stega: {
-            enabled: stegaEnabled,
-            studioUrl,
-        },
-       token : "Authorization: Bearer sknWtK4sRtdBnGntwSiu8QA4bjYRark5Zh5c9xxKdOP7CaXbdVQeOYAHJSVNt4OGbOjAQIYcWuaCE36TdnuL01UyUv5N0zlTQC7vfxrjSqJY4GLRjobBsAoBKzN7kcl6wPrkR8AIi0BN6Wz3VucDNrct0CrzyMIZiddRDyHC4fIpEvZJibOj"
-    });
-
-    // handle "GET" request
-    exportDataset({
-        // Instance of @sanity/client configured to correct project ID and dataset
-        client: client,
-
-        // Name of dataset to export
-        dataset: 'production',
-
-        // Path to write tar.gz-archive file to, or `-` for stdout
-        outputPath: '/Users/leoneldomingos/myDataset.tar.gz',
-
-        // Whether or not to export assets. Note that this operation is currently slightly lossy;
-        // metadata stored on the asset document itself (original filename, for instance) might be lost
-        // Default: `true`
-        assets: false,
-
-        // Exports documents only, without downloading or rewriting asset references
-        // Default: `false`
-        raw: true,
-
-        // Whether or not to export drafts
-        // Default: `true`
-        drafts: false,
-
-        // Export only given document types (`_type`)
-        // Optional, default: all types
-        types: ['products', 'variants'],
-
-
-
-        // Run 12 concurrent asset downloads
-        assetConcurrency: 12,
-
-        // What mode to use when exporting documents, can be eiter `stream`(default) or `cursor`.
-        // Cursor mode might help when dealing with large datasets, but might yield inconsistent results if the dataset is mutated during export.
-        // Default: 'stream'
-        mode: 'stream',
-    })
-    return json({success: true}, 200);*/
-    return result
 };
