816e298f9bf28aa869f4f2262d40a105a7268526
Lrmdom <lmatiasdomingos@gmail.com>
2025-04-08 22:41:18 +0100

    just a commit

--- app/components/toBuyVariant.tsx
+++ app/components/toBuyVariant.tsx
@@ -2,6 +2,9 @@ import type {SanityDocument} from '@sanity/client'
 import {stegaClean} from "@sanity/client/stega"
 import * as React from "react";
 import {useTranslation} from "react-i18next";
+import {CommerceLayer} from "@commercelayer/sdk";
+import AddToCartButton from "@commercelayer/react-components/orders/AddToCartButton";
+import {OrderStorage} from "@commercelayer/react-components";
 
 
 export default function ToBuyVariant({selectedSku}: { attribute: SanityDocument }) {
@@ -40,7 +43,7 @@ export default function ToBuyVariant({selectedSku}: { attribute: SanityDocument 
                     <cl-price-amount type="price"></cl-price-amount>
                 </cl-price>
                 <div>
-                    <cl-add-to-cart code={stegaClean(selectedSku)} quantity="1" kind="sku">
+                    <cl-add-to-cart code={stegaClean(selectedSku)} quantity="1" kind="sku" _external_price={true}>
                         {t('Add to cart')}
                     </cl-add-to-cart>
                 </div>
